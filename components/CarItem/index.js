2cf8c7bdcb651a53f6f2f1f9a18a10e264d07f31
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-07-27 00:36:56 -0400

    Add props with styling.

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -4,31 +4,38 @@ import styles from "./style"
 import StyledButton from "../StyledButton"
 
 const CarItem = (props) => {
+  const {name, tagLine, tagLineCTA, image} = props;
+  
   return (
     <View style={styles.carContainer}>
       <ImageBackground
-        source={require("../../assets/images/ModelS.jpeg")}
+        source={image}
         style={styles.image}
       />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>{tagLine}&nbsp;
+        <Text style={styles.subtitleCTA}>{tagLineCTA}</Text>
+        </Text>
+        
       </View>
 
-      <StyledButton 
-      type="primary"
-      content={"Custom Order"} 
-      onPress={() => {
-        console.warn('Custom Order')
-      }}/>
+      <View style={styles.buttonsContainer}>
+            <StyledButton 
+          type="primary"
+          content={"Custom Order"} 
+          onPress={() => {
+            console.warn('Custom Order')
+          }}/>
 
-      <StyledButton 
-      type="secondary"
-      content={"Existing Inventory"} 
-      onPress={() => {
-        console.warn('Existing Inventory')
-      }}/>
+          <StyledButton 
+          type="secondary"
+          content={"Existing Inventory"} 
+          onPress={() => {
+            console.warn('Existing Inventory')
+          }}/>
+          </View>
     </View>
   )
 }
